22dc958fd0ebbf214eef0a890fb1b29f13782eed
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-15 16:34:27 +0200

    FEAT: header items changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Link } from "gatsby";
+import { Link } from 'gatsby'
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.png'
+import avatar from '../assets/images/yo_BN.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <Link to="/" className="image avatar"><img src={avatar} alt="" /></Link>
-                    <h1>A super simple responsite site<br />template <strong>powered by <a href="https://conradlin.com/blog/posts/host-gatsbyjs-blog-with-notion-cms-and-netlify-for-free/" target="_blank">Notion</a></strong>,<br /> 
-                    crafted by <a href="https://conradlin.com" target="_blank">Conrad Lin</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <Link to="/" className="image avatar">
+            <img src={avatar} alt="" />
+          </Link>
+          <h1>
+            Blog que narra mi experiencia como Desarrollador Frontend junior
+            mientras busco trabajo.
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
